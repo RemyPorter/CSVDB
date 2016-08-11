c5073296a4f65615696fb451672cba084ee384d9
Remy Porter <remyporter@jetpackshark.com>
2016-08-11 08:15:02 -0500

    Started a Trie implementation which will form the backbone of the data storage model.

---
+++ CSVBase/structures/trie.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+from io import StringIO
+class Item:
+    """
+    A basic trie node class, implemented using dicts and lists.
+    >>> i = Item(1)
+    >>> i.append([2, 3, 4])
+    >>> i.append([2,4,5])
+    >>> print(str(i))
+    1
+     2
+      3
+       4
+      4
+       5
+    <BLANKLINE>
+    """
+    def __init__(self, value=None):
+        self.value = value
+        self.children = dict()
+
+    def append(self, children):
+        if children is None or len(children) == 0:
+            return
+        start = children[0]
+        rest = children[1:]
+        if not start in self.children:
+            root = Item(start)
+            self.children[root.value] = root
+        self.children[start].append(rest)
+
+    def __getitem__(self, key):
+        return self.children[key]
+
+    def __build_str__(self, out, level=0):
+        tabs = "".join([' ']*level)
+        text = "{0}{1}\n".format(tabs, self.value)
+        out.write(str(text))
+        for k,v in self.children.items():
+            v.__build_str__(out, level+1)
+
+    def __str__(self):
+        out = StringIO()
+        self.__build_str__(out)
+        return out.getvalue()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
