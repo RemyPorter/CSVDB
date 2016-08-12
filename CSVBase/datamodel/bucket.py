b9f7d5626e158bb7c9a7e9863d6f2820e07c831e
Remy Porter <remyporter@jetpackshark.com>
2016-08-12 18:34:23 -0400

    Created the bucket, a wrapper around the trie. Buckets are the foundational data structure of the system.

---
+++ CSVBase/datamodel/bucket.py
@@ -0,0 +1,20 @@
+from .structures.trie import Item
+class Bucket:
+    def __init__(self, name):
+        self.name = name
+        self._data = Item(name)
+
+    def create(self, row):
+        self._data.append(row)
+
+    def read(self, query_row):
+        return self._data.search(query_row)
+
+    def update(self, query_row, new_data):
+        self._data.remove(query_row)
+        self._data.append(new_data)
+
+    def delete(self, query_row):
+        self._data.remove(query_row)
+
+
