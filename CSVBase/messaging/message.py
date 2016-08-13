1adb8e41ef02241cc2f74d4f168e43e058592862
Remy Porter <remyporter@jetpackshark.com>
2016-08-12 20:23:58 -0400

    Added tests just for clarity on the message type.

--- CSVBase/messaging/message.py
+++ CSVBase/messaging/message.py
@@ -3,6 +3,10 @@ class Message:
 
     Operation is the key used by subscribers to
     be notified of the message.
+
+    >>> m = Message("dummy_create", row=[1, 2, 3])
+    >>> m["row"]
+    [1, 2, 3]
     """
     def __init__(self, operation, **kwargs):
         self.operation = operation
@@ -10,3 +14,7 @@ class Message:
 
     def __getitem__(self, key):
         return self.data[key]
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
