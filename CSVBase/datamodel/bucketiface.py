d6984bcaf84d06c898552c88ad6d3a2e854c7db6
Remy Porter <remyporter@jetpackshark.com>
2016-08-14 20:38:39 -0400

    A bucket interface object which simply acts as a manager of buckets. Supports create/drop operations.

---
+++ CSVBase/datamodel/bucketiface.py
@@ -0,0 +1,46 @@
+from .bucket import Bucket
+from .bucketmessenger import bucket_message
+class DuplicateBucketError(Exception):
+    pass
+
+class BucketInterface:
+    """
+    Wrap the set of active buckets and support messages
+    for accessing those buckets.
+
+    >>> from ..messaging.bus import InternalBus
+    >>> from ..messaging.message import Message
+    >>> bus = InternalBus()
+    >>> bi = BucketInterface(bus)
+    >>> m = Message("system_create_bucket", name="foo")
+    >>> m["name"]
+    'foo'
+    >>> bus.publish(None, m)
+    >>> bi.buckets["foo"].name
+    'foo'
+    """
+    def __init__(self, bus):
+        self.buckets = dict()
+        bus.subscribe("system_create_bucket",
+            getattr(self, "_create_message"))
+        bus.subscribe("system_drop_bucket",
+            getattr(self, "_drop_message"))
+        self._bus = bus
+
+    def create_bucket(self, name):
+        if name in self.buckets.keys():
+            raise DuplicateBucketError()
+        self.buckets[name] = bucket_message(Bucket(name), self._bus)
+
+    def drop_bucket(self, name):
+        del self.buckets[name]
+
+    def _create_message(self, message):
+        self.create_bucket(message["name"])
+
+    def _drop_message(self, message):
+        self.drop_bucket(message["name"])
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
